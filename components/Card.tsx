3ea73ebcd3fc7b8bac827a615124ca8ce790268c
Ashwin I S <i.s.ashwin2006@gmail.com>
2026-06-19 22:09:14 +0530

    add dashboard module

---
+++ components/Card.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Models } from "node-appwrite";
+import { useState } from "react";
+import Thumbnail from "@/components/Thumbnail";
+import { convertFileSize } from "@/lib/utils";
+import FormattedDateTime from "@/components/FormattedDateTime";
+import ActionDropdown from "@/components/ActionDropdown";
+import FilePreview from "@/components/FilePreview";
+
+const Card = ({ file }: { file: Models.Document }) => {
+  const [showPreview, setShowPreview] = useState(false);
+
+  return (
+    <>
+      <div
+        onClick={() => setShowPreview(true)}
+        className="file-card"
+      >
+        <div className="flex justify-between">
+          <Thumbnail
+            type={file.type}
+            extension={file.extension}
+            url={file.url}
+            className="!size-20"
+            imageClassName="!size-11"
+          />
+
+          <div className="flex flex-col items-end justify-between">
+            <ActionDropdown file={file} />
+            <p className="body-1">{convertFileSize(file.size)}</p>
+          </div>
+        </div>
+
+        <div className="file-card-details">
+          <p className="subtitle-2 line-clamp-1">{file.name}</p>
+          <FormattedDateTime
+            date={file.$createdAt}
+            className="body-2 text-light-100"
+          />
+          <p className="caption line-clamp-1 text-light-200">
+            By: {file.owner.fullName}
+          </p>
+        </div>
+      </div>
+
+      <FilePreview
+        file={file}
+        isOpen={showPreview}
+        onClose={() => setShowPreview(false)}
+      />
+    </>
+  );
+};
+export default Card;
